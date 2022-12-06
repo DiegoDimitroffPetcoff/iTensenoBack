9c9f0a2dd3bf014baa0f184e7829d65d7c0d95c5
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-12-06 09:50:46 +0000

    Change Expiration time.

--- SRC/controllers/controllers.js
+++ SRC/controllers/controllers.js
@@ -12,8 +12,7 @@ class Controllers {
     // if (req.isAuthenticated()) {
     let data = {
       allTech: await TechModel.find(),
-      allusers: await UserModel.find()
-      
+      allusers: await UserModel.find(),
     };
 
     // console.log(data);
@@ -35,36 +34,36 @@ class Controllers {
 
   async getUserHome(req, res) {
     if (req.isAuthenticated()) {
-      console.log("REDIRECCIONADO A RUTA GET> /USERHOME")
-    let data = {
-      allTech: await TechModel.find(),
-      allusers: await UserModel.find()
-    };
-    res.json(data);
+      console.log("REDIRECCIONADO A RUTA GET> /USERHOME");
+      let data = {
+        allTech: await TechModel.find(),
+        allusers: await UserModel.find(),
+      };
+      res.json(data);
     } else {
       res.json("Usuario no logeado");
     }
   }
   async postUserHome(req, res) {
     if (req.isAuthenticated()) {
-            let userUpdate = await UserModel.updateOne(
+      let userUpdate = await UserModel.updateOne(
         {
           username: req.body.SearchByName,
         },
         {
           $set: {
-            firstName:req.body.firstName,
-            lastName:req.body.lastName,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
             aboutMe: req.body.aboutMe,
-            socialNet:req.body.socialNet,
-            phonenumber:req.body.phonenumber,
+            socialNet: req.body.socialNet,
+            phonenumber: req.body.phonenumber,
           },
         }
       );
       console.log(req.body.SearchByName);
-console.log(userUpdate);
+      console.log(userUpdate);
 
-    res.redirect("userHome");
+      res.redirect("userHome");
     } else {
       res.json("Usuario no logeado");
     }
@@ -80,53 +79,101 @@ console.log(userUpdate);
     // }
   }
 
+  async addTechToUser(req, res) {
+    // console.log(req.body.name.toUpperCase());
+    // if (req.isAuthenticated()) {
+    let techSearched = await TechModel.findOne({
+      name: req.body.tech.toUpperCase(),
+    });
+
+    let userSearched = await UserModel.findOne({
+      username: req.body.name,
+    });
+    console.log(techSearched.users);
+    let userAdded = techSearched.users.push(userSearched._id);
+    console.log(techSearched.users);
+
+    let tech = await TechModel.updateOne(
+      {
+        name: req.body.tech.toUpperCase(),
+      },
+      {
+        $set: {
+          users: techSearched.users,
+        },
+      }
+    );
+
+    res.redirect("userHome");
+    //   } else {
+    //     res.json("Usuario no logeado");
+    //   }
+  }
+
   async searchTech(req, res) {
     // console.log(req.body.name.toUpperCase());
     // if (req.isAuthenticated()) {
     let techSearched = await TechModel.findOne({
-      name: req.body.name.toUpperCase(),
+      name: req.body.tech.toUpperCase(),
     });
-    // console.log(techSearched);
-    res.json(techSearched);
+    console.log(techSearched.users);
+    let allusers = [];
+    const map1 = techSearched.users.map(async x =>
+      allusers.push(
+      await UserModel.find({
+        _id: techSearched.users,
+      })
+// return allusers
+    )
+
+    );
+    console.log(allusers);
+    res.send(map1);
     //   } else {
     //     res.json("Usuario no logeado");
     //   }
   }
   async vote(req, res) {
-
     if (req.isAuthenticated()) {
-
-    let user = await UserModel.findOne({
-      // Se envia por el body el nombre a puntuar: {name: Laura}
-      username: req.body.name,
-    });
-    let VotoNuevo = req.body.VotoNuevo;
-    let SumaDeVotosH = await user.points.SumaDeVotosH;
-    let VotantesCantidad = await user.points.VotantesCantidad;
-    let Averange = await user.points.Averange;
-
-    let prueba = Stars(VotoNuevo, SumaDeVotosH, VotantesCantidad);
-    // actualizacion del usuario
-    let userUpdate = await UserModel.updateOne(
-      {
+      let user = await UserModel.findOne({
+        // Se envia por el body el nombre a puntuar: {name: Laura}
         username: req.body.name,
-      },
-      {
-        $set: {
-          points: {
-            SumaDeVotosH: prueba.SumaDeVotosH,
-            VotantesCantidad: prueba.VotantesCantidad,
-            Averange:prueba.Averange
-          },
+      });
+      let VotoNuevo = req.body.VotoNuevo;
+      let SumaDeVotosH = await user.points.SumaDeVotosH;
+      let VotantesCantidad = await user.points.VotantesCantidad;
+      let Averange = await user.points.Averange;
+
+      let prueba = Stars(VotoNuevo, SumaDeVotosH, VotantesCantidad);
+      // actualizacion del usuario
+      let userUpdate = await UserModel.updateOne(
+        {
+          username: req.body.name,
         },
-      }
-    );
-    console.log(userUpdate);
-    res.json(prueba);
-      } else {
-        res.json("Usuario no logeado");
-      }
+        {
+          $set: {
+            points: {
+              SumaDeVotosH: prueba.SumaDeVotosH,
+              VotantesCantidad: prueba.VotantesCantidad,
+              Averange: prueba.Averange,
+            },
+          },
+        }
+      );
+      console.log(userUpdate);
+      res.json(prueba);
+    } else {
+      res.json("Usuario no logeado");
+    }
   }
 }
 
 module.exports = Controllers;
+
+// FUNCION PARA ENCONTRAR EL USUARIO
+
+// let searched = await UserModel.findOne({
+//   _id: userSearched._id
+// });
+// console.log(userSearched._id);
+// console.log(searched);
